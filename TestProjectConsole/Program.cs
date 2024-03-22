e61edce47c6353d3e3862bae82778c081bea0cdd
Anastasiia <bzovanastia@gmail.com>
2024-03-22 19:45:29 +0100

    v1.2

--- TestProjectConsole/Program.cs
+++ TestProjectConsole/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestProjectConsole
@@ -11,6 +12,15 @@ namespace TestProjectConsole
         static void Main(string[] args)
         {
             Console.WriteLine("Testowy projekt");
+
+            Console.WriteLine("Randomiser");
+            Random random = new Random();
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(random.Next());
+            }
 
         }
     }
